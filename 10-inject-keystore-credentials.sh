cb669dfd484b611f5bc7cf4f647fe11e4ea4dd9f
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-05-09 14:07:34 +0200

    Use parenthesis to execute command, not curly brackets

--- 10-inject-keystore-credentials.sh
+++ 10-inject-keystore-credentials.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export SPRING_APPLICATION_JSON="${cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | jq -r '. | {difi:{move:{org:{keystore:.}}}}'}"
+export SPRING_APPLICATION_JSON="$(cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | jq -r '. | {difi:{move:{org:{keystore:.}}}}')"
