89e7aec28002ea182413a084cdf1cb516e8be88a
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-05-23 10:43:32 +0200

    -o is not a flag in the linux base64 command

--- 20-decode-virkcert.sh
+++ 20-decode-virkcert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-base64 -i /var/run/secrets/nais.io/virksomhetssertifikat/key.p12.b64 -o /tmp/key.p12
+base64 -d /var/run/secrets/nais.io/virksomhetssertifikat/key.p12.b64 > /tmp/key.p12
 chmod 400 /tmp/key.p12
