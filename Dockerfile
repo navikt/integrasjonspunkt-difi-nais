5c57bebca89d2ec1c0580d3cd654f99a37c7e359
Sigurd Midttun <sigurd.midttun@nav.no>
2019-09-03 16:24:48 +0200

    Use java8

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM navikt/java:11
+FROM navikt/java:8
 
 EXPOSE 8080
 
--- spring-datasource-hikari-vault/patch.sh
+++ spring-datasource-hikari-vault/patch.sh
@@ -5,5 +5,5 @@ mkdir -p workdir/BOOT-INF/lib
 cp target/dependency/*.jar workdir/BOOT-INF/lib/
 cp target/*.jar workdir/BOOT-INF/lib/
 cd workdir || exit 1
-wget -O app.jar https://beta-meldingsutveksling.difi.no/content/repositories/itest/no/difi/meldingsutveksling/integrasjonspunkt/2.0.0-SNAPSHOT/integrasjonspunkt-2.0.0-20190830.084655-20.jar
+wget -O app.jar https://beta-meldingsutveksling.difi.no/content/repositories/itest/no/difi/meldingsutveksling/integrasjonspunkt/2.0.0-SNAPSHOT/integrasjonspunkt-2.0.0-20190902.095254-23.jar
 zip -r -0 app.jar BOOT-INF/
