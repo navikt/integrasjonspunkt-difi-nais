4d1c7f55526fbfea507b86843142a16e4f84269e
Sigurd Midttun <sigurd.midttun@nav.no>
2019-09-23 10:25:05 +0200

    Updated to release 2.0.0

--- spring-datasource-hikari-vault/patch.sh
+++ spring-datasource-hikari-vault/patch.sh
@@ -5,5 +5,5 @@ mkdir -p workdir/BOOT-INF/lib
 cp target/dependency/*.jar workdir/BOOT-INF/lib/
 cp target/*.jar workdir/BOOT-INF/lib/
 cd workdir || exit 1
-wget -O app.jar https://beta-meldingsutveksling.difi.no/service/local/repositories/itest/content/no/difi/meldingsutveksling/integrasjonspunkt/2.0.0-SNAPSHOT/integrasjonspunkt-2.0.0-20190904.114035-36.jar
+wget -O app.jar https://beta-meldingsutveksling.difi.no/service/local/repositories/releases/content/no/difi/meldingsutveksling/integrasjonspunkt/2.0.0/integrasjonspunkt-2.0.0.jar
 zip -r -0 app.jar BOOT-INF/
