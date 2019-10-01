48bf844022eab05e5e0cc11f3795245fdff47114
Erik Braten <erik.braten@nav.no>
2019-10-01 09:32:21 +0200

    JKS keystore (midlertidig workaround)

--- 10-inject-keystore-credentials.sh
+++ 10-inject-keystore-credentials.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-export SPRING_APPLICATION_JSON="$(cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | jq -r '. | {difi:{move:{org:{keystore:.}}}}')"
+# bruk jks keystore (workaround for https://github.com/bcgit/bc-java/issues/586)
+#export SPRING_APPLICATION_JSON="$(cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | jq -r '. | {difi:{move:{org:{keystore:.}}}}')"
+export SPRING_APPLICATION_JSON="$(cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | sed 's/pkcs12/jks/' | jq -r '. | {difi:{move:{org:{keystore:.}}}}')"
--- 20-decode-virkcert.sh
+++ 20-decode-virkcert.sh
@@ -2,3 +2,11 @@
 
 base64 -d /var/run/secrets/nais.io/virksomhetssertifikat/key.p12.b64 > /tmp/key.p12
 chmod 400 /tmp/key.p12
+
+# - opprett jks keystore fra pkcs12 keystore (workaround for https://github.com/bcgit/bc-java/issues/586) -
+SRC_ALIAS="$(cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | jq -r '.alias')"
+SRC_PASSWORD="$(cat /var/run/secrets/nais.io/virksomhetssertifikat/credentials.json | jq -r '.password')"
+
+keytool -importkeystore -srckeystore /tmp/key.p12 -srcstoretype pkcs12 -srcstorepass $SRC_PASSWORD -srcalias "$SRC_ALIAS" -destkeystore /tmp/key.jks -deststoretype jks -deststorepass $SRC_PASSWORD -destalias "$SRC_ALIAS"
+chmod 400 /tmp/key.jks
+# ---
