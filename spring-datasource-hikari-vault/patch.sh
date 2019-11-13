e85f47f991e6cd27d20155cf1d8df9d45d7f5145
Erik Braten <erik.braten@nav.no>
2019-11-13 12:35:02 +0100

    Update to 2.0.7-SNAPSHOT to test queue read blob fix

--- spring-datasource-hikari-vault/patch.sh
+++ spring-datasource-hikari-vault/patch.sh
@@ -5,5 +5,5 @@ mkdir -p workdir/BOOT-INF/lib
 cp target/dependency/*.jar workdir/BOOT-INF/lib/
 cp target/*.jar workdir/BOOT-INF/lib/
 cd workdir || exit 1
-wget -O app.jar https://beta-meldingsutveksling.difi.no/service/local/repositories/releases/content/no/difi/meldingsutveksling/integrasjonspunkt/2.0.6/integrasjonspunkt-2.0.6.jar
+wget -O app.jar https://beta-meldingsutveksling.difi.no/service/local/repositories/itest/content/no/difi/meldingsutveksling/integrasjonspunkt/2.0.7-SNAPSHOT/integrasjonspunkt-2.0.7-20191113.112710-6.jar
 zip -r -0 app.jar BOOT-INF/
