50446e3c01329112e7335c26435d5684a6e66520
Erik Braten <erik.braten@nav.no>
2019-11-15 14:16:05 +0100

    Remove ws logging

--- 12-soap-logging.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export JAVA_OPTS="$JAVA_OPTS -Dcom.sun.xml.ws.transport.http.client.HttpTransportPipe.dump=true -Dcom.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump=true -Dcom.sun.xml.ws.transport.http.HttpAdapter.dump=true -Dcom.sun.xml.internal.ws.transport.http.HttpAdapter.dump=true -Dcom.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold=999999"
